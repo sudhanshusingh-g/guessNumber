bcbe37e2ce13f5546c205617494511a85c218ec1
Sudhanshu Singh <sudsingh32@gmail.com>
2023-12-08 17:17:35 +0530

    Guess Number game

---
+++ script.js
@@ -0,0 +1,64 @@
+const randomNumber=parseInt(Math.floor(Math.random()*10)+1)
+const button=document.querySelector('#button')
+const input=document.querySelector('#input')
+const prevGuess=document.querySelector('.prevGuess')
+const attempt=document.querySelector('.attempt')
+const result=document.querySelector('.result')
+const resultContainer=document.querySelector('.resultContainer')
+
+const error=document.createElement('p')
+
+
+
+const guessList=[]
+let attemptNumber=1;
+let playGame=true;
+
+
+if(playGame){
+    button.addEventListener('click',(e)=>{
+        e.preventDefault()
+        const value=input.value
+        validateInput(value)
+    })
+}
+
+// Check input is number or not
+function validateInput(value){
+    if( isNaN(value) || value ==='' || value < 1 || value > 10){
+        result.innerHTML=`<h2>Please enter a valid number 🙏🏻`
+    }
+    else{
+        checkGuess(value)
+    }
+}
+
+// Check the guess whether it is in the range, if yes, resultDisplay will be called
+function checkGuess(guess){
+    guessList.push(guess)
+    prevGuess.innerHTML+=`${guess},`
+    if(guess === randomNumber){
+        result.innerHTML=`You win`
+        endGame()
+    }
+
+}
+
+// To display result of the guess, update the attempt, and store the previous guess values
+function resultDisplay(guess){
+    
+}
+
+// Display message
+function displayMessage(message){
+
+}
+
+// start a game 
+function newGame(){
+
+} 
+// end game
+function endGame(){
+    
+}
